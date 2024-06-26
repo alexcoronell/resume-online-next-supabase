cba33bdaacf3ee2c6c55ac59d55a331ce8b41e90
Alex Coronell <alexander@catapultsteel.com>
2024-06-26 14:09:31 -0500

    Basic structure header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,46 @@
 export default function Header() {
+
+  const menu = [
+    {
+      title: 'Home',
+      url: '/'
+    },
+    {
+      title: 'Portfolio',
+      url: '/portfolio'
+    },
+    {
+      title: 'Studies',
+      url: '/studies'
+    },
+    {
+      title: 'Training',
+      url: '/training'
+    },
+    {
+      title: 'Experiences',
+      url: '/experiences'
+    },
+    {
+      title: 'Contact',
+      url: '/Contact'
+    },
+  ]
+
   return (
     <header>
-      MAIN MENU
+      <button>Open/Close Menu</button>
+      <nav>
+        <ul>
+          {
+            menu.map(item => (
+              <li>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
     </header>
   );
 }
