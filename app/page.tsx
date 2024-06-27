589604255f39e46058eabb0009359b78649269bf
Alex Coronell <alexander@catapultsteel.com>
2024-06-27 11:13:53 -0500

    Home page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,37 @@
+import Image from "next/image";
+import { blurData } from "@/core/data/blurData";
 import getPersonalData from "@/core/services/personal-data.service";
 
 export default async function Index() {
   const personalData = await getPersonalData();
 
   return (
-    <article>
-      <h1>
-        {personalData.firstname} {personalData.lastname}
-      </h1>
-      <h2>{personalData.title}</h2>
-      <p>{personalData.description}</p>
-      <img
-        src={personalData.image}
-        alt={
-          personalData.firstname +
-          " " +
-          personalData.lastname +
-          " profile image"
-        }
-      />
+    <article className="w-full p-4 overflow-y-scroll h-full">
+      <div className="h-full lg:grid lg:grid-cols-3 lg:items-center lg:gap-x-12">
+        <div className="w-[328px] h-[328px] overflow-hidden relative rounded-full mx-auto xl:w-[400px] xl:h-[400px]">
+          <Image
+            src={personalData.image}
+            alt={
+              personalData.firstname +
+              " " +
+              personalData.lastname +
+              " profile image"
+            }
+            fill={true}
+            priority={true}
+            placeholder="blur"
+            blurDataURL={blurData}
+          />
+        </div>
+        <div className="lg:col-span-2 lg:pl-6">
+          <h1 className="py-3 lg:text-left">
+            {personalData.firstname} {personalData.lastname}
+          </h1>
+          <h2 className="mb-3 lg:text-left">{personalData.title}</h2>
+          <p className="text-center leading-7 lg:text-left">{personalData.description}</p>
+        </div>
+      </div>
+      {/* <img src={personalData.image} alt="test" /> */}
     </article>
   );
 }
