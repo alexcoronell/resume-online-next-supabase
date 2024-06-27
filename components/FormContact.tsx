52c0ac086fd9329098589c42a7f0fddfc00224c1
Alex Coronell <alexander@catapultsteel.com>
2024-06-27 18:27:51 -0500

    Form Contact basic layout

--- components/FormContact.tsx
+++ components/FormContact.tsx
@@ -1,9 +1,39 @@
+"use client";
+
+import styles from '../styles/formContact.module.css'
+
 export default function FormContact() {
-    return(
-        <article>
-            <form>
-                Form
-            </form>
-        </article>
-    )
+  return (
+    <article className={styles.FormContact}>
+      <form>
+        <div className={styles.formgroup}>
+          <label htmlFor="name">
+            <input type="text" name="name" id="name" placeholder="name" />
+            <span>Name</span>
+          </label>
+        </div>
+        <div className='flex gap-3'>
+        <div className={styles.formgroup}>
+          <label htmlFor="email">
+            <input type="email" name="email" id="email" placeholder="email" />
+            <span>Email</span>
+          </label>
+        </div>
+        <div className={styles.formgroup}>
+          <label htmlFor="phone">
+            <input type="text" name="phone" id="phone" placeholder="phone" />
+            <span>Phone</span>
+          </label>
+        </div>
+        </div>
+        <div className={styles.formgroup}>
+          <label htmlFor="message">
+            <textarea name="message" id="message" placeholder="message" rows={4} ></textarea>
+            <span>Message</span>
+          </label>
+        </div>
+        <button className={styles.btnSubmit}>Send</button>
+      </form>
+    </article>
+  );
 }
--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -6,9 +6,8 @@ import SocialMediaItem from "./SocialMediaItem";
 
 export default async function SocialMedia() {
   const contacts: Contact[] = await getContacts();
-  console.log(contacts);
   return (
-    <article className="bg-backgroundsecondary p-10 rounded-3xl h-[470px] overflow-y-auto flex flex-col justify-center w-full max-lg:max-w-[400px] mx-auto">
+    <article className="bg-backgroundsecondary p-10 rounded-3xl h-[470px] overflow-y-auto flex flex-col justify-center w-full max-lg:max-w-[400px] mx-auto lg:h-[500px]">
       {contacts.map((contact) => (
         <SocialMediaItem key={contact.id} contact={contact} />
       ))}
