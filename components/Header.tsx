5db78934ad2473954fa23deab1bb37cd3985ae43
Alex Coronell <alexander@catapultsteel.com>
2024-06-28 09:35:07 -0500

    Text size adjusted in menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,61 +1,68 @@
-"use client"
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
 /* Components */
-import { MajesticonsMenu } from './ui/MajesticonsMenu';
-import { MajesticonsClose } from './ui/MajesticonsClose';
+import { MajesticonsMenu } from "./ui/MajesticonsMenu";
+import { MajesticonsClose } from "./ui/MajesticonsClose";
 
 /* Styles */
-import styles from '../styles/header.module.css'
+import styles from "../styles/header.module.css";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => setIsOpen(!isOpen);
+  const handleClick = (active: boolean) => setIsOpen(active);
 
   const menu = [
     {
-      title: 'Home',
-      url: '/'
+      title: "Home",
+      url: "/",
     },
     {
-      title: 'Portfolio',
-      url: '/portfolio'
+      title: "Portfolio",
+      url: "/portfolio",
     },
     {
-      title: 'Studies',
-      url: '/studies'
+      title: "Studies",
+      url: "/studies",
     },
     {
-      title: 'Training',
-      url: '/training'
+      title: "Training",
+      url: "/training",
     },
     {
-      title: 'Experiences',
-      url: '/experiences'
+      title: "Experiences",
+      url: "/experiences",
     },
     {
-      title: 'Contact',
-      url: '/contact'
+      title: "Contact",
+      url: "/contact",
     },
-  ]
+  ];
 
   return (
     <header className={styles.Header}>
-      <button className='openCloseMenuBtn' onClick={handleClick}><MajesticonsMenu className='size-12 text-primary' /></button>
-      <nav id='menu' className={isOpen ? 'right-0' : 'right-[-100%]'}>
+      <button className="openCloseMenuBtn" onClick={() => handleClick(true)}>
+        <MajesticonsMenu className="size-12 text-primary" />
+      </button>
+      <nav id="menu" className={isOpen ? "right-0" : "right-[-100%]"}>
         <div>
-          <button onClick={handleClick} className='openCloseMenuBtn'><MajesticonsClose className='size-12 text-primary' /></button>
+          <button
+            onClick={() => handleClick(false)}
+            className="openCloseMenuBtn"
+          >
+            <MajesticonsClose className="size-12 text-primary" />
+          </button>
         </div>
         <ul>
-          {
-            menu.map((item, index) => (
-              <li key={index}>
-                <Link onClick={handleClick} href={item.url}>{item.title}</Link>
-              </li>
-            ))
-          }
+          {menu.map((item, index) => (
+            <li key={index}>
+              <Link onClick={() => handleClick(false)} href={item.url}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
