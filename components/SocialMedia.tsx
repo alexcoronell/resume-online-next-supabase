9f0be71ffb1aa2666b7ff6829429510b76a95c4f
Alex Coronell <alexander@catapultsteel.com>
2024-06-28 15:13:49 -0500

    checking contacts prev assign variable

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -9,9 +9,10 @@ interface ContactViewProps {
 }
 
 export default function SocialMedia({contacts}: ContactViewProps) {
+  const finalContacts = contacts ? contacts : []
   return (
     <article className="bg-backgroundsecondary p-10 rounded-3xl h-[470px] overflow-y-auto flex flex-col justify-center w-full max-lg:max-w-[400px] mx-auto lg:h-[500px] special-shadow">
-      {contacts.map((contact) => (
+      {finalContacts.map((contact) => (
         <SocialMediaItem key={contact.id} contact={contact} />
       ))}
     </article>
