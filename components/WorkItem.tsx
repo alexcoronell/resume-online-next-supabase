1db371150b9444760e67a64cbb5d91a642fe5613
Alex Coronell <alexander@catapultsteel.com>
2024-06-28 10:13:27 -0500

    Portfolio page and items completed

--- components/WorkItem.tsx
+++ components/WorkItem.tsx
@@ -9,7 +9,11 @@ import getimageUrl from "@/helpers/getImagesUrl";
 /* Models */
 import { Work } from "@/core/models/Work.interface";
 
+/* Blur Data */
 import { blurData } from "@/core/data/blurData";
+
+/* Styles */
+import styles from "../styles/work-item.module.css";
 
 interface WorkViewProps {
   work: Work;
@@ -31,8 +35,8 @@ export default async function WorkItem({ work }: WorkViewProps) {
   const skills = technologies.split(",");
   const imageUrl = await getimageUrl(bucketName, image);
   return (
-    <article className="p-1 max-lg:border max-lg:border-primary pb-6 sm:p-6 lg:bg-backgroundsecondary h-full">
-      <div className="relative mx-auto flex item justify-center w-[340px] h-[238px] sm:w-[518px] sm:h-[352px] lg:w-[420px] lg:h-[295px] xl:w-[524px] xl:h-[352px]">
+    <article className={styles.WorkItem + ' special-shadow'}>
+      <div className={styles.WorkItem__imageArea}>
         <Image
           src={imageUrl}
           alt={title}
@@ -40,26 +44,29 @@ export default async function WorkItem({ work }: WorkViewProps) {
           placeholder="blur"
           blurDataURL={blurData}
         />
-        {/* <img
-          src={imageUrl}
-          alt={title}
-        /> */}
       </div>
-      <div className="grid grid-rows-3 gap-y-3">
-      <h3 className="my-4 grow-0">{title}</h3>
-      <div className="flex flex-wrap gap-3 justify-center">
-        {skills.map((skill, index) => (
-          <SkillItem key={index} skill={skill} />
-        ))}
-      </div>
-      <div className="pt-3 py-1 mt-auto flex items-center justify-center grow-0 gap-6">
-        {
-          url && (<a className="py-4 px-8 border border-primary rounded-3xl text-lg text-primary hover:text-background hover:bg-primary" href={url} target="_blank">Visit</a>)
-        }
-        {
-          publicRepo && (<a className="py-4 px-8 border border-primary rounded-3xl text-lg text-primary hover:text-background hover:bg-primary" href={repoUrl} target="_blank">Visit the code</a>)
-        }
-      </div>
+      <div className={styles.WorkItem__details}>
+        <div className="py-1 grow-0">
+          <h3>{title}</h3>
+          <p>Status: {status}</p>
+        </div>
+        <div className={styles.WorkItem__technologies}>
+          {skills.map((skill, index) => (
+            <SkillItem key={index} skill={skill} />
+          ))}
+        </div>
+        <div className={styles.WorkItem__buttonArea}>
+          {url && (
+            <a className="btn-primary" href={url} target="_blank">
+              Visit
+            </a>
+          )}
+          {publicRepo && (
+            <a className="btn-primary" href={repoUrl} target="_blank">
+              Visit the code
+            </a>
+          )}
+        </div>
       </div>
     </article>
   );
