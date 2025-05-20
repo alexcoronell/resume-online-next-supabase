bec08d6ba2ad6c5ab4dd8470f1c435d539e84236
Alex Coronell <alexcoronell@gmail.com>
2025-05-20 15:59:30 -0500

    HeaderButtons added on Study Admin Page

--- app/admin/studies/page.tsx
+++ app/admin/studies/page.tsx
@@ -1,6 +1,11 @@
 import SectionPage from '@/components/SectionPage';
+import { HeaderButtons } from '@/components/shared/admin/header-buttons';
 
 export default async function AdminPage() {
   const titlePage = 'Studies';
-  return <SectionPage titlePage={titlePage}>CONTENT</SectionPage>;
+  return (
+    <SectionPage titlePage={titlePage}>
+      <HeaderButtons />
+    </SectionPage>
+  );
 }
