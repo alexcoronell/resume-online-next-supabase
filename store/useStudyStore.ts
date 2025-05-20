c2569986b66ddeba445713d95def0400afe4c4a5
Alex Coronell <alexcoronell@gmail.com>
2025-05-20 16:28:29 -0500

    useStudyStorePartial completed

--- store/useStudyStore.ts
+++ store/useStudyStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { useEffect } from "react";
 
 /* Service */
 import { getStudies } from "@/core/services/study.service";
@@ -8,15 +9,34 @@ import type { Study } from "@/core/models/Study.interface";
 
 type StudyStore = {
     studies: Study[];
-    getStudies: () => Promise<Study[]>;
+    total: number;
+    currentPage: number;
+    currentPageSize: number;
+    setPage: (page: number) => void;
+    setCurrentPageSize: (pageSize: number) => void;
+    getStudies: (page?: number, pageSize?: number) => Promise<Study[]>;
 }
 
-export const useStudyStore = create<StudyStore>((set) => ({
+export const useStudyStore = create<StudyStore>((set, get) => ({
     studies: [],
-    institutes: [],
-    getStudies: async () => {
-        const studies = await getStudies();
-        set({ studies });
+    total: 0,
+    currentPage: 1,
+    currentPageSize: 10,
+    setPage: (page) => {
+        set({ currentPage: page });
+        get().getStudies();
+    },
+    setCurrentPageSize: (pageSize) => {
+        set({ currentPageSize: pageSize });
+        get().getStudies();
+    },
+    getStudies: async (page, pageSize) => {
+        const state = get();
+        const { studies, total } = await getStudies(
+            page ?? state.currentPage,
+            pageSize ?? state.currentPageSize
+        );
+        set({ studies, total });
         return studies;
-    }
+    },
 }));
