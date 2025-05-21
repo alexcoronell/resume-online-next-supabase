839740dc38dfed33e8a66d1580f6a569c83cfc88
Alex Coronell <alexcoronell@gmail.com>
2025-05-20 20:02:21 -0500

    Pagination partial layout

--- components/shared/admin/FooterPagination.tsx
+++ components/shared/admin/FooterPagination.tsx
@@ -1,3 +1,90 @@
+import React from 'react';
+import styles from '@/styles/pagination.module.css';
+
 export function FooterPagination() {
-  return <p>Pagination</p>;
+  return (
+    <div className={styles.Pagination}>
+      <ButtonFirstPage />
+      <ButtonBack />
+      <p className='px-3'>Page <span className='px-1 text-primary font-bold underline underline-offset-4'>1</span> of <span>10</span></p>
+      <ButtonNext />
+      <ButtonLastPage />
+    </div>
+  );
+}
+
+function ButtonFirstPage() {
+  return (
+    <button type='button' className={styles.ButtonDirection}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+      >
+        <title>First page</title>
+        <path
+          fill='currentColor'
+          d='M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6l6 6zM6 6h2v12H6z'
+        />
+      </svg>
+    </button>
+  );
+}
+
+function ButtonBack() {
+  return (
+    <button type='button' className={styles.ButtonDirection}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+      >
+        <title>Previous page</title>
+        <path
+          fill='currentColor'
+          d='M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6l6 6z'
+        />
+      </svg>
+    </button>
+  );
+}
+
+function ButtonNext() {
+  return (
+    <button type='button' className={styles.ButtonDirection}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+      >
+        <title>Next page</title>
+        <path
+          fill='currentColor'
+          d='M8.59 16.59L13.17 12L8.59 7.41L10 6l6 6l-6 6z'
+        />
+      </svg>
+    </button>
+  );
+}
+
+function ButtonLastPage() {
+  return (
+    <button type='button' className={styles.ButtonDirection}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+      >
+        <title>Last page</title>
+        <path
+          fill='currentColor'
+          d='M5.59 7.41L10.18 12l-4.59 4.59L7 18l6-6l-6-6zM16 6h2v12h-2z'
+        />
+      </svg>
+    </button>
+  );
 }
