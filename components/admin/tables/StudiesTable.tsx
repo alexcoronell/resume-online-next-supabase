7232b97005819fe5fa471e273a85264f724ccb68
Alex Coronell <alexcoronell@gmail.com>
2025-05-20 19:14:15 -0500

    Basic layout in studies table atyles completed

--- components/admin/tables/StudiesTable.tsx
+++ components/admin/tables/StudiesTable.tsx
@@ -7,6 +7,8 @@ import { ButtonDelete } from '@/components/shared/buttons/ButtonDelete';
 
 import { useStudyStore } from '@/store/useStudyStore';
 
+import styles from '@/styles/tablets.module.css';
+
 export const StudiesTable: FC = () => {
   const { studies, total, getStudies, currentPage, currentPageSize } =
     useStudyStore();
@@ -22,13 +24,12 @@ export const StudiesTable: FC = () => {
   }, [currentPage, currentPageSize]);
 
   const columns = [
-    'Title',
-    'Institute',
-    'Place',
-    'Since',
-    'Until',
-    'Current',
-    'Actions',
+    { title: 'Title', classes: 'text-left' },
+    { title: 'Institute', classes: 'text-left max-mdhidden' },
+    { title: 'Place', classes: 'text-left max-lg:hidden' },
+    { title: 'Since', classes: 'text-center max-xl:hidden' },
+    { title: 'Until', classes: 'text-center max-xl:hidden' },
+    { title: 'Current', classes: 'text-center max-xl:hidden' },
   ];
 
   const handleDelete = (id: string) => {
@@ -36,44 +37,50 @@ export const StudiesTable: FC = () => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column, index) => (
-            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-            <th key={index} className='text-left'>
-              {column}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {studies.map((study) => (
-          <tr key={study.id}>
-            <td>{study.title}</td>
-            <td>{study.institute}</td>
-            <td>{study.place}</td>
-            <td>{study.since}</td>
-            <td>{study.until}</td>
-            <td>{study.current ? 'Yes' : 'No'}</td>
-            <td>
-              {/* Add action buttons here */}
-              <ButtonView
-                url={`/admin/studies/${study.id}`}
-                title={`View ${study.title} details`}
-              />
-              <ButtonDelete id={study.id} deleteFunction={handleDelete}  title={`Delete ${study.title}`}/>{' '}
-            </td>
-          </tr>
-        ))}
-        {total === 0 && (
+    <div className={styles.AdminTableContainer}>
+      <table className={styles.AdminTable}>
+        <thead>
           <tr>
-            <td colSpan={columns.length} className='text-center'>
-              No studies found
-            </td>
+            {columns.map((column, index) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              <th key={index} className={column.classes}>
+                {column.title}
+              </th>
+            ))}
+            <th>Actions</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {studies.map((study) => (
+            <tr key={study.id}>
+              <td>{study.title}</td>
+              <td className='max-md:hidden'>{study.institute}</td>
+              <td className='max-lg:hidden'>{study.place}</td>
+              <td className='max-xl:hidden text-center'>{study.since}</td>
+              <td className='max-xl:hidden text-center'>{study.until}</td>
+              <td className='max-xl:hidden text-center'>{study.current ? 'Yes' : 'No'}</td>
+              <td className={styles.AdminTable__actions}>
+                <ButtonView
+                  url={`/admin/studies/${study.id}`}
+                  title={`View ${study.title} details`}
+                />
+                <ButtonDelete
+                  id={study.id}
+                  deleteFunction={handleDelete}
+                  title={`Delete ${study.title}`}
+                />{' '}
+              </td>
+            </tr>
+          ))}
+          {total === 0 && (
+            <tr>
+              <td colSpan={columns.length} className='text-center'>
+                No studies found
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
