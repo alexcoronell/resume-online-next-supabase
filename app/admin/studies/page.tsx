6cc5e07526daa3cf979a832e6a51e6c5c8c3412b
Alex Coronell <alexcoronell@gmail.com>
2025-05-20 20:09:45 -0500

    Total items partial layout

--- app/admin/studies/page.tsx
+++ app/admin/studies/page.tsx
@@ -1,6 +1,7 @@
 import SectionPage from '@/components/SectionPage';
 import { HeaderButtons } from '@/components/shared/admin/header-buttons';
 import { StudiesTable } from '@/components/admin/tables/StudiesTable';
+import { TotalItems } from '@/components/shared/admin/totalItems';
 import { FooterPagination } from '@/components/shared/admin/FooterPagination';
 
 export default async function AdminPage() {
@@ -9,6 +10,7 @@ export default async function AdminPage() {
     <SectionPage titlePage={titlePage}>
       <HeaderButtons />
       <StudiesTable />
+      <TotalItems />
       <FooterPagination />
     </SectionPage>
   );
---
+++ components/shared/admin/totalItems.tsx
@@ -0,0 +1,7 @@
+export function TotalItems() {
+    return (
+        <div className='flex items-center justify-end pb-4'>
+            <p className="pr-3">Total: <span className="font-bold text-primary">100</span></p>
+        </div>
+    )
+}
