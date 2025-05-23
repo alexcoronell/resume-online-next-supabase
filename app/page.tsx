009d68bc444d337caf15ea971532749edba0617d
Alex Coronell <alexcoronell@gmail.com>
2025-05-23 09:09:01 -0500

    personalData service renamed by profile service

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
 import Image from "next/image";
-import { cookies } from "next/headers";
 
 /* Data */
-import getPersonalData from "@/core/services/personal-data.service";
+import getProfile from "@/core/services/profile.service";
 
 /* Models */
-import { PersonalData } from '../core/models/PersonalData.interface';
+import { Profile } from '../core/models/Profile.interface';
 
 /* Styles */
 import styles from "../styles/home.module.css";
 
 export default async function Index() {
-  const personalData: PersonalData = await getPersonalData();
+  const profile: Profile = await getProfile();
   
   return (
     <article className={styles.Home + " no-scrollbar"}>
       <div className={styles.Home__container}>
         <div className={styles.Home__imageArea}>
           <Image
-            src={personalData.image}
-            alt={personalData.firstname + " " + personalData.lastname + " profile image"}
+            src={profile.image}
+            alt={profile.firstname + " " + profile.lastname + " profile image"}
             fill={true}
             priority={true}
             className="profileImage"
@@ -27,10 +26,10 @@ export default async function Index() {
         </div>
         <div className={styles.Home__detail}>
           <h1>
-            {personalData.firstname} {personalData.lastname}
+            {profile.firstname} {profile.lastname}
           </h1>
-          <h2>{personalData.title}</h2>
-          <p>{personalData.description}</p>
+          <h2>{profile.title}</h2>
+          <p>{profile.description}</p>
         </div>
       </div>
     </article>
--- core/services/profile.service.ts
+++ core/services/profile.service.ts
@@ -3,7 +3,7 @@ import { createClient } from "@/utils/supabase/client";
 
 /* Helpers */
 import getimageUrl from "@/helpers/getImagesUrl";
-import { PersonalData } from "../models/PersonalData.interface";
+import { Profile } from "../models/Profile.interface";
 
 /* Revalidate */
 export const revalidate = 60 * 60 * 24;
@@ -12,11 +12,11 @@ const supabase = createClient();
 const tableName = "profile";
 const tableBucketName = "personalData";
 
-const getPersonalData = async () => {
+const getProfile = async () => {
   const { data } = await supabase.from(tableName).select("*").single();
-  const personalData: PersonalData = data;
-  personalData.image = await getimageUrl(tableBucketName, personalData.image);
-  return await personalData;
+  const profile: Profile = data;
+  profile.image = await getimageUrl(tableBucketName, profile.image);
+  return await profile;
 };
 
-export default getPersonalData;
+export default getProfile;
