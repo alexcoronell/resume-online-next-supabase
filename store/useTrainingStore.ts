be024752248c5adb01faaebe7865c40e238c9b1b
Alex Coronell <alexcoronell@gmail.com>
2025-05-28 18:11:29 -0500

    Training store completed

--- store/useTrainingStore.ts
+++ store/useTrainingStore.ts
@@ -1,18 +1,19 @@
 import { create } from "zustand";
 
 /* Service */
-import { getStudies as fetchStudies } from "@/core/services/study.service";
+import { getTrainings as fetchTraings } from "@/core/services/training.service";
 
 /* Models */
-import type { Study } from "@/core/models/Study.interface";
+import type { Training } from "@/core/models/Training.interface";
 
 /* Types */
 import type { RequestStatus } from "@/core/types/RequestStatus.type";
 
+/* Helpers */
 import { generateOptionsLimitePage } from "@/helpers/generateOptionsLimitePage";
 
-type StudyStore = {
-    studies: Study[];
+type TrainingStore = {
+    trainings: Training[];
     total: number;
     currentPage: number;
     currentPageSize: number;
@@ -21,11 +22,11 @@ type StudyStore = {
     optionsLimit: { value: string | number; label: string }[];
     setPage: (page: number) => void;
     setCurrentPageSize: (pageSize: number) => void;
-    getStudies: (page?: number, pageSize?: number) => Promise<Study[]>;
+    getTrainings: (page?: number, pageSize?: number) => Promise<Training[]>;
 }
 
-export const useStudyStore = create<StudyStore>((set, get) => ({
-    studies: [],
+export const useTrainingStore = create<TrainingStore>((set, get) => ({
+    trainings: [],
     total: 0,
     currentPage: 1,
     currentPageSize: 5,
@@ -34,31 +35,31 @@ export const useStudyStore = create<StudyStore>((set, get) => ({
     optionsLimit: [],
     setPage: (page) => {
         set({ currentPage: page });
-        get().getStudies();
+        get().getTrainings();
     },
     setCurrentPageSize: (pageSize) => {
         set({ currentPageSize: pageSize });
-        if(get().total < (get().currentPage - 1) * get().currentPageSize) {
+        if (get().total < (get().currentPage - 1) * get().currentPageSize) {
             set({ currentPage: 1 });
         }
-        get().getStudies();
+        get().getTrainings();
     },
-    getStudies: async () => {
+    getTrainings: async () => {
         set({ requestStatus: 'loading' });
         const state = get();
-        const { studies, total } = await fetchStudies(
+        const { trainings, total } = await fetchTraings(
             state.currentPage,
             state.currentPageSize
         );
         set({ optionsLimit: generateOptionsLimitePage(total) });
         set({ totalPages: Math.ceil(total / state.currentPageSize) });
-        set({ studies, total });
+        set({ trainings, total });
         set({ requestStatus: 'success' });
-        if(studies === null) {
+        if (trainings === null) {
             set({ requestStatus: 'failed' });
             set({ currentPage: 1 });
-            get().getStudies();
+            get().getTrainings();
         }
-        return studies;
+        return trainings;
     },
 }));
