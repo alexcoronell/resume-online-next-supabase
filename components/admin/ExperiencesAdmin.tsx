f8065f0d850f8251f43bfb8a0b9416b07c4e6f4a
Alex Coronell <alexcoronell@gmail.com>
2025-06-05 09:53:37 -0500

    Experiences admin component completed

--- components/admin/ExperiencesAdmin.tsx
+++ components/admin/ExperiencesAdmin.tsx
@@ -1,3 +1,38 @@
+'use client';
+import { HeaderButtons } from '../shared/admin/header-buttons';
+import { ExperiencesTable } from './tables/ExperiencesTable';
+import { TotalItems } from '../shared/admin/totalItems';
+import { FooterPagination } from '../shared/admin/FooterPagination';
+
+/* Store */
+import { useExperienceStore } from '@/store/useExperienceStore';
+
 export function ExperiencesAdmin() {
-    return <h1>ExperiencesAdmin</h1>
+  const {
+    getExperiences,
+    total,
+    optionsLimit,
+    setCurrentPageSize,
+    currentPage,
+    totalPages,
+    setPage,
+  } = useExperienceStore();
+  const createUrl = '/admin/portfolio/create';
+  return (
+    <div className='PortfolioAdmin'>
+      <HeaderButtons
+        createUrl={createUrl}
+        refresh={getExperiences}
+        optionsLimit={optionsLimit}
+        setCurrentPageSize={setCurrentPageSize}
+      />
+      <ExperiencesTable />
+      <TotalItems total={total} />
+      <FooterPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setPage={setPage}
+      />
+    </div>
+  );
 }
