79fa4f7cd351f6d36eb968d60ff56cf94ff956b5
Alex Coronell <alexcoronell@gmail.com>
2025-11-20 21:59:43 -0500

    Training show filter applied on Courses page

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -19,7 +19,7 @@ export const dynamic = 'force-dynamic';
 
 export default async function Trainings() {
   const supabase = createClient();
-  const { data } = await supabase.from("trainings").select("*, institute(*)").order('created_at', { ascending: false });
+  const { data } = await supabase.from("trainings").select("*, institute(*)").eq('show', true).order('created_at', { ascending: false });
   const trainings: Training[] = await orderByYearAndMonth(data);
   const titlePage = "Courses";
 
